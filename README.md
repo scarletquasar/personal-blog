cb009f5f3f007078649475d97b3e57750121f815
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2023-09-30 00:19:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# personal-svelte-blog
+ 
---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<main>
+  <nav>
+    <h1>Scarlet Rose</h1>
+  </nav>
+  <div class="sub-nav">
+    <h2>Software Architect</h2>
+  </div>
+  <div class="sub-nav">
+    <h3>architecture / performance / algorithms</h3>
+  </div>
+  <section>
+    aa
+  </section>
+</main>
