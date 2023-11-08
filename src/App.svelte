ac3daebca273697eeb83caa15b36bb167546ee18
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2023-11-08 02:01:46 -0300

    frontend updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,55 @@
 <main>
   <nav>
-    <h1>Scarlet Rose</h1>
+    <img width="45" height="45" src="https://img.icons8.com/ios/50/FFFFFF/lambda.png" alt="lambda"/>
+    <h1>Scarlet Victoria</h1>
   </nav>
-  <div class="sub-nav">
-    <h2>Software Architect</h2>
-  </div>
-  <div class="sub-nav">
-    <h3>architecture / performance / algorithms</h3>
-  </div>
-  <section>
-    aa
+  <section id="introduction">
+    <h1>About me</h1>
+
+    <p>
+      Hello. My name is Scarlet Victoria Rose, but I usually go for only Scarlet or Victoria. I am a
+      software developer with <b>7+</b> years of hands on experience with software development,
+      architecture, leadership and product development from zero to deployment. During my experience time
+      I have dealt with a lot of different situations - I've worked in 3 different fintechs, one bank, an
+      ERP application, a radiation management device ecosystem and much more.
+    </p>
+
+    <p>
+      My passion is to create tooling and libraries, I am an active open-source contributor in my free time and 
+      very interested in matters like natural language processing, graph discovery and manipulation algorithms,
+      utility-focused web crawling and automation. I've been coding since I was 12 and I see the development work
+      as a thing that I want to do for my life.
+    </p>
+
+    <p>
+      My technical background is huge - I had to deal with several languages in my carreer, the most remarkable
+      ones were: C#, F#, Golang, Rust, JavaScript/TypeScript, Zig, C/C++ and a little of Erlang.
+      I am also an academic person, got a technology bachelor's degree in Information Systems
+      in 2022 and I'm going for a bachelor's degree in Software Engineering with ending prevision for 2027. After that,
+      my objectives are getting a master's degree and in a long term future, a PhD in mathematics.
+    </p>
+
+    <h1>My work</h1>
+
+    <p>
+      Currently, as previously mentioned, I work in a lot of different things a time. I like to write 
+      (articles, books, documentation...), contribute in open-source projects and participate in lectures and programming
+      communities.
+    </p>
+
+    <h2>Lectures</h2>
+
+
+
+    <div id="cityjs" class="work-card">
+      <div>
+        <h2>CityJS Brazil Lecture</h2>
+        <h3>March 2023</h3>
+        <h4>
+          Presented a quick lecture about the DX impact of modern techcnial tooling in teams. Talked
+          about my JavaScript runtime project "Melon".
+        </h4>
+      </div>
+    </div>
   </section>
 </main>
