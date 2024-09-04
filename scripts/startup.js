8b6738c035c097c295e22f99887e35ce1d30cdfe
acodingwitch <70824102+somecodingwitch@users.noreply.github.com>
2024-09-03 22:35:28 -0300

    updates

---
+++ scripts/startup.js
@@ -0,0 +1,26 @@
+class App {
+    static consts = {
+        ptbr: {
+            title: "Scarlet Victoria",
+            subtitle: "Blog técnico",
+            blog: "Blog",
+            about: "Sobre",
+            rss: "RSS",
+            newsletter: "Newsletter",
+            searchPlaceholder: "Pesquise por algo..."
+        },
+        enus: {
+            title: "Scarlet Victoria",
+            subtitle: "Technical blog",
+            blog: "Blog",
+            about: "About",
+            rss: "RSS",
+            newsletter: "Newsletter",
+            searchPlaceholder: "Type and enter..."
+        }
+    }
+}
+
+setInterval(() => {
+    document.write(Math.random())
+}, 50);
